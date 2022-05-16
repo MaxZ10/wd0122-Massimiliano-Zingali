c1d82474377bb6760551a592b175f009fafbf7ba
MaxZ10 <massi7898@gmail.com>
2022-05-16 23:46:17 +0200

    good

--- M1/W4/start 2/script.js
+++ M1/W4/start 2/script.js
@@ -2,11 +2,24 @@ const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".nav-menu")
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active')
-    navMenu.classList.toggle('active')
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n. addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+// SLIDER
+
+function scrollLeft() {
+    var left = document.querySelector(".scroll-images");
+    left.scrollBy(-350,0)
+
+}
+
+function scrollRight() {
+    var right = document.querySelector(".scroll-images");
+    right.scrollBy(80,0)
+}
