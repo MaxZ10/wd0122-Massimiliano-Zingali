a03b49c0891082a0dc87119c917f9bdc3b0c1ff1
MaxZ10 <massi7898@gmail.com>
2022-05-30 10:20:04 +0200

    fixCard

---
+++ M1/W3/D2/script.js
@@ -0,0 +1,49 @@
+
+alert('Ciao sono la tua nuova Calcolatrice di fiducia')
+
+var nomeUser = window.prompt('Come ti chiami?')
+
+
+alert('Ciao piacere di conoscerti '+ nomeUser + '!')
+
+var annoUser = window.prompt('Quanti anni hai?')
+
+if (annoUser >= 18) {
+    alert('Quindi sei il mio nuovo migliore '+ nomeUser + ' e hai addirittura '+annoUser+ ' anni'+ '!')
+}
+
+else if (annoUser <18) {
+    alert('Sei troppo piccolo!');
+    alert('Per questa volta chiudo un occhio');
+}
+var displayEl = document.getElementById('input-calc').value;
+var acEl = document.getElementsByClassName('.ac');
+var pmEl = document.getElementsByClassName('.pm');
+var percentEl = document.getElementsByClassName('.percent');
+
+var additionEl = document.getElementsByClassName('.addition');
+var subtractionEl = document.getElementsByClassName('.subtraction');
+var multiplicationEl = document.getElementsByClassName('.multiplication');
+var divisionEl = document.getElementsByClassName('.division');
+var equalEl = document.getElementsByClassName('.equal');
+
+var number0El = document.getElementsByClassName('.number-0');
+var number1El = document.getElementsByClassName('.number-1');
+var number2El = document.getElementsByClassName('.number-2');
+var number3El = document.getElementsByClassName('.number-3');
+var number4El = document.getElementsByClassName('.number-4');
+var number5El = document.getElementsByClassName('.number-5');
+var number6El = document.getElementsByClassName('.number-6');
+var number7El = document.getElementsByClassName('.number-7');
+var number8El = document.getElementsByClassName('.number-8');
+var number9El = document.getElementsByClassName('.number-9');
+
+
+// FUNCTION
+
+
+
+
+
+ 
+
---
+++ M2/D5/script.js
@@ -0,0 +1,51 @@
+
+const cards = document.querySelectorAll(".card");
+
+let matched = 0;
+
+let cardOne, cardTwo;
+
+let disableDeck = false;
+
+function flipCard({target: clickedCard}) {
+    if(cardOne !== clickedCard && !disableDeck) {
+        clickedCard.classList.add("flip");
+        if(!cardOne) {
+            return cardOne = clickedCard;
+        }
+        cardTwo = clickedCard;
+        disableDeck = true;
+        let cardOneImg = cardOne.querySelector(".back-view img").src,
+        cardTwoImg = cardTwo.querySelector(".back-view img").src;
+        matchCards(cardOneImg, cardTwoImg);
+    }
+}
+function matchCards(img1, img2) {
+    if(img1 === img2) {
+        matched++;
+        if(matched == 8) {
+            setTimeout(() => {
+                return shuffleCard();
+            }, 1000);
+        }
+        cardOne.removeEventListener("click", flipCard);
+        cardTwo.removeEventListener("click", flipCard);
+        cardOne = cardTwo = "";
+        return disableDeck = false;
+    }
+    setTimeout(() => {
+        cardOne.classList.add("shake");
+        cardTwo.classList.add("shake");
+    }, 400);
+    setTimeout(() => {
+        cardOne.classList.remove("shake", "flip");
+        cardTwo.classList.remove("shake", "flip");
+        cardOne = cardTwo = "";
+        disableDeck = false;
+    }, 1200);
+}
+
+    
+cards.forEach(card => {
+    card.addEventListener("click", flipCard);
+});
