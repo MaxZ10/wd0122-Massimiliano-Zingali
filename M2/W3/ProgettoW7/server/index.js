e7c073f72727794057bab452a5e511e2c9ebc7df
MaxZ10 <massi7898@gmail.com>
2022-06-13 12:26:02 +0200

    comm

---
+++ M2/W3/ProgettoW7/server/index.js
@@ -0,0 +1,63 @@
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+let count = 4;
+let users = users.json;
+
+//GET
+
+app.get('/api/users', (request, response) => {
+    response.json(users);
+})
+
+app.get('/api/users/:id', (request, response) => {
+    const id = request.params.id;
+    users.forEach(ele => {
+        if(ele.id === +id) {
+            response.json(ele);
+            return;
+        }
+    })
+})
+
+// POST
+
+app.post('/api/users', (request, response) => {
+    const obj = request.body;
+    obj.id = count++;
+    users.push(obj);
+    response.send('Utente Aggiunto nel DB');
+})
+
+// PUT
+app.put('/api/users/:id', (request, response) => {
+    const id = request.params.id;
+    const obj_mod = request.body;
+
+ 
+    let index = users.findIndex(ele => ele.id === +id);
+    users.splice(index, 1, obj_mod);
+
+
+
+    response.send('Utente Modificato nel DB');
+})
+
+// DELETE
+app.delete('/api/users/:id', (request, response) => {
+    const id = request.params.id;
+    
+    users = users.filter(ele => ele.id !== +id);
+   
+    response.send('Utente Eliminato dal DB');
+})
+
+
+app.listen(3000, () => console.log('Server attivo sulla porta 3000'))
