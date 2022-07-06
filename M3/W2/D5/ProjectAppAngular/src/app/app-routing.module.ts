3ec220f2f9cb29f61aa47f2ee8920aba8e146a46
MaxZ10 <massi7898@gmail.com>
2022-07-06 13:02:53 +0200

    s

--- M3/W2/D5/ProjectAppAngular/src/app/app-routing.module.ts
+++ M3/W2/D5/ProjectAppAngular/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountsPage } from './pages/accounts/accounts.page';
+import { HomePage } from './pages/home/home.page';
+import { UsersPage } from './pages/users/users.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'accounts',
+    component: AccountsPage
+  },
+  {
+    path: 'home',
+    component: HomePage,
+  },
+  {
+    path:'users',
+    component: UsersPage
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- M3/W2/D5/ProjectAppAngular/src/app/app.module.ts
+++ M3/W2/D5/ProjectAppAngular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePage } from './pages/home/home.page';
 import { UsersPage } from './pages/users/users.page';
+import { AccountsPage } from './pages/accounts/accounts.page';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePage,
-    UsersPage
+    UsersPage,
+    AccountsPage
   ],
   imports: [
     BrowserModule,
