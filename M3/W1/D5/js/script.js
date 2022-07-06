ca46d43211128c15530168578325302b8c67e7fe
MaxZ10 <massi7898@gmail.com>
2022-07-06 09:25:48 +0200

    z

---
+++ M3/W1/D5/js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+alert("Cabina Telefonica!");
+
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+
+const updateTime = () => {
+    const currentTime = new Date();
+  
+    let currentHour = currentTime.getHours();
+    const currentMinute = currentTime.getMinutes();
+  
+    if (currentHour > 24) {
+      currentHour -= 24;
+    }
+    hourEl.textContent = currentHour.toString();
+    minuteEl.textContent = currentMinute.toString().padStart(2, '0');
+  }
+  setInterval(updateTime, 1000);
+  updateTime();
+  
---
+++ M3/W1/D5/ts/script.ts
@@ -0,0 +1,2 @@
+alert("Cabina Telefonica!")
+
